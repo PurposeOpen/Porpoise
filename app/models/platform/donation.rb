3d7d246ed97940429c93ee604904ecf1cdcd846b
Diego Marcet <diego.marcet@tipit.net>
2013-04-26 15:26:40 -0300

    [diego] - Fixed API endpoint concatenation for custom find method on Donation

--- app/models/platform/donation.rb
+++ app/models/platform/donation.rb
@@ -4,6 +4,15 @@ class Platform::Donation < Platform::Base
 	def self.find_by_subscription_id(subscription_id)
 		# The find method using the :first scope requires the web service to return a collection.
 		# To avoid changing the Platform API to fit this requirement, we use the :one scope which requires specifying the :from option
-		find(:one, :from => "#{self.site}/donations", :params => {:subscription_id => subscription_id})
+		find(:one, :from => find_by_subscription_id_path, :params => {:subscription_id => subscription_id})
+	end
+
+	private
+	def self.find_by_subscription_id_path
+		base_uri = self.site.to_s
+		if !base_uri.match(/\/$/)
+			base_uri << '/'
+		end
+		URI.join(base_uri, 'donations').to_s
 	end
 end
