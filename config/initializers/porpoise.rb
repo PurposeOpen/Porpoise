be0c77ec9c5c20e42ba82b119ecb3301f5473e56
smarcet <smarcet@gmail.com>
2013-06-07 18:33:39 -0300

    [smarcet] - Put in filters to ensure CC information isn't logged

--- config/initializers/porpoise.rb
+++ config/initializers/porpoise.rb
@@ -1,3 +1,5 @@
+Rails.application.config.filter_parameters += [:card_number, :card_cvv2, :card_expiration_month, :card_expiration_year]
+
 Rails.application.config.to_prepare do
   Platform.configure
 end
